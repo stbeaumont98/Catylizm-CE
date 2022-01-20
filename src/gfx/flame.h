9f2605428b4ea6db3a10335c79968edeb1a1c8ef
Spencer Beaumont <spence.beaumont@gmail.com>
2022-01-20 03:02:12 -0500

    Removed the old flame sprites and created a single flame sprite that can be flipped programmatically.

---
+++ src/gfx/flame.h
@@ -0,0 +1,18 @@
+#ifndef flame_include_file
+#define flame_include_file
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define flame_width 4
+#define flame_height 4
+#define flame_size 18
+#define flame_compressed_size 18
+extern unsigned char flame_compressed[18];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
